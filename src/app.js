f5fabe6119cf7d5d8ebd1b9bef258de2f6671bd6
robertoalvesferreira <robertoaff06@gmail.com>
2020-02-11 20:54:46 -0300

    Criação do modelo e migration do usuario assim como rota de inserção do usuario 'teste'

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-expressions */
+import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
+import './database';
 
 class App {
   constructor() {
---
+++ src/database/migrations/20200211232745-create-user.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('Users');
+  },
+};
