1e61ab0c74878ccbcafa5e0ce3525d0ea1f90a14
robertoalvesferreira <robertoaff06@gmail.com>
2020-02-11 21:23:22 -0300

    Configurando autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controller/UserController';
+import authMiddleware from './app/middlewares/auth';
+import SessionController from './app/controller/SessionController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
 routes.post('/users', UserController.store);
 
 export default routes;
